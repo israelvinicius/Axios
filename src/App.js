0458d1460fb112c3a5a1feac45e16670170ed0f2
Israel Vinicius <israelvinicius14@hotmail.com>
2018-09-27 16:36:32 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <botão onClick = { this .ping}> Ping! </ button> 
+      </div>
+    );
+  }
+}
+
+export default App;
